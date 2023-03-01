bbef79ef0f5af81e7cea7033663832e6dd656c82
Rodolpho Nunes da Silva Rodrigues <rodolpho.vlgph@gmail.com>
2023-03-01 17:55:33 -0300

    ok

---
+++ variaveis/script.js
@@ -0,0 +1,42 @@
+var nome = 'Rodolpho';
+var idade = 28;
+var possuiFaculdade = true;
+console.log(nome, idade, possuiFaculdade)
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+console.log(preco,totalComprado,totalPreco)
+
+var sobrenome = "Rafael", cidade = "Rio de Janeiro", idade2 = 43;
+console.log(sobrenome, cidade, idade2)
+
+var semDefinir;
+console.log(semDefinir)
+
+var time = 'Corinthians'
+console.log(time)
+time = 'Corinthians2'
+console.log(time)
+
+let time2 = 'CorinthiansLet'
+console.log(time2)
+time2 = 'CorinthiansLet2'
+console.log(time2)
+
+const senhaUsuario = 'abcd12345678'
+console.log(senhaUsuario)
+// senhaUsuario = 'mudei'
+// console.log(senhaUsuario)
+
+var nomeEx = 'Rodolpho';
+var idadeEx =  28;
+var comidaFav;
+comidaFav = 'Macarrao com carne moida';
+var variavel1,
+    variavel2,
+    variavel3,
+    variavel4,
+    variavel5;
+
+console.log(nomeEx, idadeEx, comidaFav,variavel1, variavel2, variavel3, variavel4, variavel5)
