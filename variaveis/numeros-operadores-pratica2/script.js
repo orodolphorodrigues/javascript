a32ab9191ed4be9520b1b8d69f6fe5b3dce42b9b
Rodolpho Nunes da Silva Rodrigues <rodolpho.vlgph@gmail.com>
2023-03-02 16:48:53 -0300

    update

---
+++ variaveis/numeros-operadores-pratica2/script.js
@@ -0,0 +1,32 @@
+var idade = 2e10;
+
+console.log(idade);
+
+var soma = 100 + 50;
+console.log('soma de 100 + 50 =',soma);
+var substracao = 100 - 50;
+console.log('substracao de 100 - 50 =',substracao);
+var multiplicacao = 100 * 2;
+console.log('multiplicacao 100 * 2 =',multiplicacao);
+var divisao = 100 / 2;
+console.log('divisao de 100 / 2 =',divisao);
+var expoente = 2 ** 4; //2*2*2*2
+console.log('expoente de 2 ** 4 =',expoente);
+var modulo = 14 % 5; //resto da divisao
+console.log('modulo de 14 % 5 =',modulo) ;
+
+
+var somaString = '100' + 50
+console.log('Soma de string e numero',somaString);
+
+var subtracaoString = '100' - 50;
+console.log('Subtracao de string e numero', subtracaoString)
+
+var multiplicacaoString = '100' * 3;
+console.log('Multiplicacao de string e numero', multiplicacaoString)
+
+var divisaoString = '100' / 5;
+console.log('Divisao de string e numero', divisaoString)
+
+var testeNan = 'aqui tem 100' + 10;
+console.log(testeNan)
